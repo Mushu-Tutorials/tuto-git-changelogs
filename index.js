499b8ef3defae9ac5322b055344439dcc3d86838
MushuLeDragon <dragon.mushu@hotmail.com>
2020-12-21 22:19:53 +0100

    feature: Add Push command to script JS

--- index.js
+++ index.js
@@ -102,18 +102,19 @@ if (hotfix.length) {
   newChangelog += '\n';
 }
 
-// prepend the newChangelog to the current one
+// Prepend the newChangelog to the current one
 fs.writeFileSync('./CHANGELOG.md', `${newChangelog}${currentChangelog}`);
 
-// update package.json
+// Update the package.json file
 fs.writeFileSync(
   './package.json',
   JSON.stringify({ version: String(newVersion) }, null, 2)
 );
 
-// create a new commit
+// Create a new commit
 child.execSync('git add .');
 child.execSync(`git commit -m "chore: Bump to version ${newVersion}"`);
-
-// tag the commit
+// Tag the commit
 child.execSync(`git tag -a -m "Tag for version ${newVersion}" v${newVersion}`);
+// Push the code and tags
+child.execSync(`git push --tags`);
