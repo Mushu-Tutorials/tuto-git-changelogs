f06d9a66c0a70ab802223d2be2bd883e17fc4146
MushuLeDragon <dragon.mushu@hotmail.com>
2020-12-21 08:12:44 +0100

    feature: Change the version format in the project. Use dot like 1.0.6.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const child = require('child_process');
 const fs = require('fs');
 
-const latestTag = child.execSync('git describe --long').toString('utf-8').split('-')[0];
+const latestTag = child
+  .execSync('git describe --long')
+  .toString('utf-8')
+  .split('-')[0];
 const output = child
   .execSync(`git log ${latestTag}..HEAD --format=%B%H----DELIMITER----`)
-  .toString("utf-8");
+  .toString('utf-8');
 
 const commitsArray = output
   .split('----DELIMITER----\n')
@@ -15,9 +18,16 @@ const commitsArray = output
   })
   .filter((commit) => Boolean(commit.sha));
 
+// Write in the CHANGELOG.md file
 const currentChangelog = fs.readFileSync('./CHANGELOG.md', 'utf-8');
-const currentVersion = Number(require('./package.json').version);
-const newVersion = currentVersion + 1;
+
+// Update the package.json file version with Dot (last element)
+const currentVersion = require('./package.json').version;
+const newVersion = Number(currentVersion.split('.').slice(-1)[0]) + 1;
+// Update the package.json file version with Int Number
+// const currentVersion = Number(require('./package.json').version);
+// const newVersion = currentVersion + 1;
+
 let newChangelog = `# Version ${newVersion} (${
   new Date().toISOString().split('T')[0]
 })\n\n`;
@@ -68,11 +78,16 @@ if (chores.length) {
 fs.writeFileSync('./CHANGELOG.md', `${newChangelog}${currentChangelog}`);
 
 // update package.json
-fs.writeFileSync("./package.json", JSON.stringify({ version: String(newVersion) }, null, 2));
+fs.writeFileSync(
+  './package.json',
+  JSON.stringify({ version: String(newVersion) }, null, 2)
+);
 
 // create a new commit
 child.execSync('git add .');
 child.execSync(`git commit -m "chore: Bump to version ${newVersion}"`);
 
 // tag the commit
-child.execSync(`git tag -a -m "Tag for version ${newVersion}" version${newVersion}`);
+child.execSync(
+  `git tag -a -m "Tag for version ${newVersion}" version${newVersion}`
+);
